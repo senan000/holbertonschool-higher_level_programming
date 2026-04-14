2805b5f1808d40d3b049cbbd01e8a882f108f2ec
senan000 <13737@holbertonstudents.com>
2026-04-14 15:49:54 +0000

    FINAL FINAL PASS

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-str = "Holberton"
-print(str[3:] + str[:3])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
