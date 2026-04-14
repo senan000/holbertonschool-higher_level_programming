7d2f77062c7f368e0e4dcbcf6dff323b874d8522
senan000 <13737@holbertonstudents.com>
2026-04-14 16:07:57 +0000

    add import modules tasks

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    count = len(argv)
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(count))
+    for i in range(count):
+        print("{:d}: {:s}".format(i + 1, argv[i]))
