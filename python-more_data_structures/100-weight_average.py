8cb901f01ba49d16b15d190f0dc166ba52d743e1
senan000 <13737@holbertonstudents.com>
2026-04-27 20:52:53 +0000

    Fix weight_average function

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -4,10 +4,4 @@ def weight_average(my_list=[]):
         return 0
     total = sum(x * y for x, y in my_list)
     weights = sum(y for x, y in my_list)
-    return total / weights1~#!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    total = sum(x * y for x, y in my_list)
-    weights = sum(y for x, y in my_list)
     return total / weights
